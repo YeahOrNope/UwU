ec279bcba44fe7a02fc3831d4907d3cf75dd9db4
YeahOrNope <148880342+YeahOrNope@users.noreply.github.com>
2024-01-16 14:41:36 +0000

    ffszgvb

--- backend/main.ts
+++ backend/main.ts
@@ -1,4 +1,12 @@
-import {} from "https://deno.land/x/oak/mod.ts"
-Deno.serve((_request: Request) => {
-  return new Response("Hello, world!");
-})
+import {Application, Router } from "https://deno.land/x/oak@v12.6.2/mod.ts"
+//Deno.serve((_request: Request) => {
+//  return new Response("Hello, world!");
+//})
+
+const app = new Application();
+
+app.use((ctx) => {
+  ctx.response.body = "Hello World!";
+});
+
+await app.listen({ port: 8000 });
