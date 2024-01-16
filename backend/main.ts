149f6618dc06f92430fccd60b74a6d1814ba1412
YeahOrNope <148880342+YeahOrNope@users.noreply.github.com>
2024-01-16 15:42:17 +0000

    cuiams

--- backend/main.ts
+++ backend/main.ts
@@ -1,48 +1,47 @@
-import {Application, Router } from "https://deno.land/x/oak@v12.6.2/mod.ts"
-//Deno.serve((_request: Request) => {
-//  return new Response("Hello, world!");
-//})
+import { Application, Router } from "https://deno.land/x/oak@v12.6.2/mod.ts"
+import { Status } from "https://deno.land/x/oak_commons@0.4.0/status.ts";
 
 const app = new Application();
 const router = new Router();
 
 router.get("/", (ctx) => {
-  ctx.response.body = "Bober sus hihihi"
+  ctx.response.body = "Hi mom!"
 })
 
 router.get("/add/:a/:b", (ctx) => {
-  const {a, b } = ctx.params
-  console.log(a,b)
-  ctx.response.status = Status.NoConten
+  const { a, b } = ctx.params
+  const value = Number(a) + Number(b)
+  ctx.response.body = value
 })
 
-const dict: {[Key: string]:string } = {}
-//const dict: Record<string, string> = {}
+const dict: Record<string, string> = {}
 
 router.post("/dict/set/:key/:value", (ctx) => {
-  const {key, value } = ctx.params
-  dict [key] = value
+  const { key, value } = ctx.params
+  dict[key] = value
   ctx.response.status = Status.NoContent
 })
 
-router.post("/dict/get/:key", (ctx) => {
-  const { key } = CacheStorage.params
-  CacheStorage.response.body = dict{key}
+router.get("/dict/get/:key", (ctx) => {
+  const { key } = ctx.params
+  ctx.response.body = dict[key]
 })
 
 const kv = await Deno.openKv()
 router.post("/register", async (ctx) => {
-  const body = ctx.request.body({type: "json"})
   try {
-    const body = context.request.body({ type: "json"})
-    const credentials = await body.value
-    console.log(credentials.login, credentials.password)
+      const body = ctx.request.body({ type: "json" })
+      const credentials = await body.value
+      const key = ["users", credentials.login]
+      const value = { password: credentials.password }
+      await kv.set(key, value)
+      ctx.response.status = Status.NoContent
   } catch {
-    context.response.status = Status.Unauthorized
+      ctx.response.status = Status.Unauthorized
   }
 })
 
-app.use(Router.routes())
-app.use(Router.allowedMethods())
+app.use(router.routes())
+app.use(router.allowedMethods())
 
 await app.listen({ port: 8000 });
