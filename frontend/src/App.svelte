283f77d07ca02a66ba87fa6050a22daea3730165
YeahOrNope <148880342+YeahOrNope@users.noreply.github.com>
2024-02-20 14:52:34 +0000

    ifazdc

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,11 +1,22 @@
 <script lang="ts">
   let response: String;
 
+  let a: number = 0;
+  let b: number = 0;
+  
   async function sendRequest() {
     const res = await fetch("/api", {
       method: "GET",
     });
 
+    return res.text();
+  }
+
+  async function addNumbers(a: number, b: number) {
+
+    const res = await fetch(`/api/add/${a}/${b}`, {
+      method: "GET";
+    })
     return res.text();
   }
 
@@ -17,5 +28,15 @@
 <main>
   {response}
 
-  <button on:click={getHome}>Send request</button>
+  <button on:click={() => addNumbers(2,4)}>Send request</button>
+  {response}
+
+  A = <input type="number" bind:value={a} />
+  B = <input type="number" bind:value={b} />
+  <button on:click={async () => {
+    response = await addNumbers(a,b);
+  }} >Send request</button>
+
+  A + B = {respose}
 </main>
+
